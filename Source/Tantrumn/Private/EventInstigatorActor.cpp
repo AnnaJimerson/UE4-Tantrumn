44039328b3ff3446e5d6b58903bfc60236b5f217
AnnaJimerson <ajimerson@activision.com>
2022-08-25 05:10:52 -0700

    Added Interface for Event Triggers, created more BP's, did some level design

---
+++ Source/Tantrumn/Private/EventInstigatorActor.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EventInstigatorActor.h"
+
+// Sets default values
+AEventInstigatorActor::AEventInstigatorActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEventInstigatorActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEventInstigatorActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AEventInstigatorActor::TriggerActorTargets()
+{
+	// If already triggered return
+	if (Triggered) return;
+
+	// Print that this triggered
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("%s Triggered An Event!"), *GetName()));
+
+	// Trigger all actor targets
+	for (AActor* target : TriggerTargets) 
+	{
+		// Then trigger everything using an interface (For some reason we use 'U' for checking and 'I' for calling???)
+		// why????
+		if (target->Implements<UEventTrigger>()) {
+			IEventTrigger::Execute_ReactToTrigger(target);
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, FString::Printf(TEXT("%s affected!"), *target->GetName()));
+		}
+	}
+
+	// Set triggered once done
+	Triggered = true;
+}
+
---
+++ Source/Tantrumn/Public/EventInstigatorActor.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EventTrigger.h"
+#include "EventInstigatorActor.generated.h"
+
+UCLASS()
+class TANTRUMN_API AEventInstigatorActor : public AActor, public IEventTrigger
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEventInstigatorActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EventActor")
+		TArray<AActor*> TriggerTargets;
+
+	UFUNCTION(BlueprintCallable)
+		void TriggerActorTargets();
+
+	UPROPERTY(BlueprintReadWrite)
+		bool Triggered;
+
+};
